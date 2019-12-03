d03c58e913cb375bbfcc79d7c3e03a4bb1446d58
adelina92f <adelina_92f@abv.bg>
2019-12-03 19:55:31 +0200

    website

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,6 @@
 $(function(){
     let $nav = $('.navigation');
-    let $toggleButton = $('.hamburger');
+    let $toggleButton = $('.toggle-menu_2');
     $toggleButton.on('click', function(e){
         $toggleButton.toggleClass('is-active');
         $nav.toggleClass('open')
@@ -9,7 +9,7 @@ $(function(){
 
 $(function(){
     let $topHolder = $('.top-holder');
-    let $toggleHamburger = $('.top-hamburger');
+    let $toggleHamburger = $('.toggle-menu_1');
     $toggleHamburger.on('click', function(e){
         $toggleHamburger.toggleClass('is-active');
         $topHolder.toggleClass('active')
